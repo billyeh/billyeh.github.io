88210eaca78be781bc7c61a76a250ce7f4478139
Bill Yeh <byeh@qti.qualcomm.com>
2014-06-25 15:42:59 -0700

    add nullchecks

--- timeline.js
+++ timeline.js
@@ -6,6 +6,10 @@
     var logFile = "https://dl.dropboxusercontent.com/u/30426053/hellopip.log";
     var startTime = 3000;
 
+    /*********************************************
+    Functionality
+    *********************************************/
+
     function readTextFile(file, cb) {
         var rawFile = new XMLHttpRequest();
         rawFile.open("GET", file, false);
@@ -26,7 +30,7 @@
             loc["time"] = parseInt(time.substring(time.length - 4, time.length));
         }
         function filter(loc) {
-            return loc["time"] > startTime;
+            return loc && loc["time"] && (loc["time"] > startTime);
         }
         locations = locations.map(map);
         locations = locations.filter(filter);
